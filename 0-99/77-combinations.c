ab995f8b7431a6cf7972d5f30fe5f9891074879d
snnbyyds <snnbyyds@gmail.com>
2024-09-07 20:24:34 +0800

    leetcode: Sync 2024-09-07
    Change-Id: I8a7c2883df4af78a54c46c3a613a3ce10bdc47eb

---
+++ 0-99/77-combinations.c
@@ -0,0 +1,50 @@
+/* https://leetcode.cn/problems/combinations/ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int** combine(int n, int k, int* returnSize, int** returnColumnSizes);
+void backtrack(int **ans, int n, int k, int* returnSize, int** returnColumnSizes, int *path, int top, int startidx);
+
+int main(void){
+    int n = 4;
+    int k = 2;
+    int returnSize;
+    int *returnColumnSizes;
+    int **ans = combine(n, k, &returnSize, &returnColumnSizes);
+    for (int i = 0; i < returnSize; i++)
+    {
+        for (int j = 0; j < returnColumnSizes[i]; j++)
+        {
+            printf("%d\t", ans[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int** combine(int n, int k, int* returnSize, int** returnColumnSizes){
+    int **ans = malloc(sizeof(int *) * 184756); // 184756 == 20C10
+    *returnSize = 0;
+    *returnColumnSizes = malloc(sizeof(int) * 187456);
+    int *path = malloc(sizeof(int) * k);
+    backtrack(ans, n, k, returnSize, returnColumnSizes, path, 0, 1);
+    return ans;
+}
+
+void backtrack(int **ans, int n, int k, int* returnSize, int** returnColumnSizes, int *path, int top, int startidx){
+    if (top == k)
+    {
+        ans[*returnSize] = malloc(sizeof(int) * top);
+        (*returnColumnSizes)[*returnSize] = top;
+        memcpy(ans[(*returnSize)++], path, sizeof(int) * top);
+        return;
+    }
+    for (int i = startidx; i <= n - (k - top) + 1; i++)
+    {
+        path[top++] = i;
+        backtrack(ans, n, k, returnSize, returnColumnSizes, path, top, i + 1);
+        top--;
+    }
+}
---
+++ 2300-2399/2397-maximum-rows-covered-by-columns.c
@@ -0,0 +1,80 @@
+/* https://leetcode.cn/problems/maximum-rows-covered-by-columns/ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int maximumRows(int** matrix, int matrixSize, int* matrixColSize, int numSelect);
+void backtrack(int *ans, int** matrix, int matrixSize, int* matrixColSize, int numSelect, int *path, int top, int startidx);
+int covered(int** matrix, int matrixSize, int* matrixColSize, int *path, int top);
+
+int main(void){
+    int **matrix = malloc(sizeof(int*) * 4);
+    int matrix0[3] = {0, 0, 0};
+    int matrix1[3] = {1, 0, 1};
+    int matrix2[3] = {0, 1, 1};
+    int matrix3[3] = {0, 0, 1};
+    matrix[0] = matrix0;
+    matrix[1] = matrix1;
+    matrix[2] = matrix2;
+    matrix[3] = matrix3;
+    int matrixSize = 4;
+    int matrixColSize[4] = {3, 3, 3, 3};
+    int numSelect = 2;
+    printf("%d\n", maximumRows(matrix, matrixSize, matrixColSize, numSelect));
+    return 0;
+}
+
+int maximumRows(int** matrix, int matrixSize, int* matrixColSize, int numSelect){
+    int ans = -1;
+    int *path = malloc(sizeof(int) * (numSelect + 1));
+    backtrack(&ans, matrix, matrixSize, matrixColSize, numSelect, path, 0, 0);
+    return ans;
+}
+
+void backtrack(int *ans, int** matrix, int matrixSize, int* matrixColSize, int numSelect, int *path, int top, int startidx){
+    if (top == numSelect)
+    {
+        int is_covered = covered(matrix, matrixSize, matrixColSize, path, top);
+        *ans = MAX(*ans, is_covered);
+        return;
+    }
+    for (int i = startidx; i < matrixColSize[0]; i++)
+    {
+        path[top++] = i;
+        backtrack(ans, matrix, matrixSize, matrixColSize, numSelect, path, top, i + 1);
+        top--;
+    }
+}
+
+int covered(int** matrix, int matrixSize, int* matrixColSize, int *path, int top){
+    int covered = 0;
+    bool covered_flag;
+    bool map[matrixColSize[0]];
+    memset(map, 0, matrixColSize[0] * sizeof(bool));
+    // bool *map = calloc(matrixColSize[0], sizeof(bool));
+    for (int i = 0; i < top; i++)
+    {
+        map[path[i]] = true;
+    }
+    for (int i = 0; i < matrixSize; i++)
+    {
+        covered_flag = true;
+        for (int j = 0; j < matrixColSize[i]; j++)
+        {
+            if (matrix[i][j] == 1 && map[j] == false)
+            {
+                covered_flag = false;
+                break;
+            }
+        }
+        if (covered_flag)
+        {
+            covered++;
+        }
+    }
+    return covered;
+}
